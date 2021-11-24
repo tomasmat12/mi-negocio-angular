b525abfcd2df11e36004b377961c22ccb50d8ea0
Manu Minnig <manuminnig@gmail.com>
2021-11-23 21:18:35 -0300

    Create ProductoComponent

---
+++ src/app/productos/productos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'productos',
+  templateUrl: './productos.component.html',
+  styleUrls: ['./productos.component.scss']
+})
+export class ProductosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
