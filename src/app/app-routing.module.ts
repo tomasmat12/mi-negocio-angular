c955ff4d07ebc089a1b9497f3bae5d255647cc12
Manu Minnig <manuminnig@gmail.com>
2021-11-23 21:27:02 -0300

    Created app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductosComponent } from './productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '',redirectTo: 'producto',pathMatch: 'full' },
+    { path: 'producto', component: ProductosComponent },
+    { path: 'about', component: AboutUsComponent },
+];
+   
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OfertaListaComponent } from './oferta-lista/oferta-lista.component';
 import { ProductosListaComponent } from './productos-lista/productos-lista.component';
+import { ProductosComponent } from './productos/productos.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductosListaComponent,
-    OfertaListaComponent
+    OfertaListaComponent,
+    ProductosComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
