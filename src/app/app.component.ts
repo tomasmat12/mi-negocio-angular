06daca447e1822e378dd6569308cc5f32361fa98
Matias Tomas <tomasrimolimatias@gmail.com>
2022-10-14 14:38:24 -0300

    fixs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title(title: any) {
+  title(title: 'mi-negocio-angular') {
     throw new Error('Method not implemented.');
   }
   year = new Date(). getFullYear();
