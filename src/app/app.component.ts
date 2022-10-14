2f47604d0ac6e865f20940c1eae30b791990e3e1
Matias Tomas <tomasrimolimatias@gmail.com>
2022-10-14 15:35:29 -0300

    fixs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mi-neogcio-angular' ;
+  title = 'mi-negocio-angular' ;
   year = new Date(). getFullYear();
 }
 
