1a077702b697f3d3bed8b8ccf8a5dbd9fc19541d
tomasmat12 <54420035+tomasmat12@users.noreply.github.com>
2022-11-10 19:43:18 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //apiUrl: 'https://manuminnig-app.herokuapp.com/'
   //apiUrl: 'http://localhost:3000/'
-  apiUrl : 'https://negocio-api.onrender.com'
+  apiUrl : 'https://negocio-api.onrender.com/'
 };
